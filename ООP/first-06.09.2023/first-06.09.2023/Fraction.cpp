dae9d51b1fd6e3b9a743ff447c3f516d0e02035c
gront0 <prizrak_fantoma@mail.ru>
2023-10-06 10:07:27 +0700

    Update Fraction.cpp

--- ООP/first-06.09.2023/first-06.09.2023/Fraction.cpp
+++ ООP/first-06.09.2023/first-06.09.2023/Fraction.cpp
@@ -1,1 +1,23 @@
 #include "Fraction.h"
+#include <iostream>
+
+Fraction Fraction::div(Fraction b)
+{
+    //Особый случай:
+    if (denominator == 0) {
+        std::cerr << "Fraction::div: error: b.numerator is 0, result will be 0/1.";
+    }
+    else {
+        m_denominator = denominator;
+    }
+
+    if (m_denominator < 0) {
+        m_numerator *= -1;
+        m_denominator *= -1;
+    }
+
+    Fraction result;
+    result.numerator = numerator * b.denominator;
+    result.denominator = denominator * b.numerator;
+    return result;
+}
