058b18aa030a41c9f2459012d65708f80e9821eb
gront0 <prizrak_fantoma@mail.ru>
2023-10-06 10:07:22 +0700

    Update Fraction.h

--- ООP/first-06.09.2023/first-06.09.2023/Fraction.h
+++ ООP/first-06.09.2023/first-06.09.2023/Fraction.h
@@ -1,9 +1,28 @@
 #pragma once
+
 class Fraction
 {
 public:
-	void printFraction(Fraction fraction);
+    Fraction();
+    Fraction(const int numerator, const int denominator);
+    ~Fraction() = default;
 
-	int numerator;
-	int denominator;
+    int getNumerator();
+    int getDenominator();
+
+    void setNumerator(int value);
+    void setDenominator(int value);
+    
+    void print() const;
+    
+    Fraction sum(Fraction b); // пример: c = a.sum(b)
+    Fraction sub(Fraction b);
+    Fraction mul(Fraction b);
+    Fraction div(Fraction b);
+    
+    void reduce();
+    
+private:
+  int m_numerator = 0; // Вторая очередь инициализации выполняется только при пропущенной первойэг
+  int m_denominator = 1; 
 };
