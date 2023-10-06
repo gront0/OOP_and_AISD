baad2e4938cd2d597591b5bad5f2ad13b64636b1
gront0 <prizrak_fantoma@mail.ru>
2023-10-06 11:49:19 +0700

    Update №1.cpp

--- AISD/№1/№1/№1.cpp
+++ AISD/№1/№1/№1.cpp
@@ -25,3 +25,16 @@ void fillMatrix(vector<vector<int>>& matrix) {
         }
     }
 }
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    srand(static_cast<unsigned int>(time(0)));
+
+    int n = 5; // количество городов
+    int startCity = 0; // начальный город
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+    fillMatrix(matrix);
+
+    return 0;
+}
