0b004042095bbffaaa250b552a96e25b3ca4a0e5
gront0 <prizrak_fantoma@mail.ru>
2023-10-06 11:40:52 +0700

    Update №1.cpp

--- AISD/№1/№1/№1.cpp
+++ AISD/№1/№1/№1.cpp
@@ -1,1 +1,27 @@
-﻿
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <locale.h>
+
+using namespace std;
+
+// генерация случайного числа в заданном диапазоне
+int randomInt(int min, int max) {
+    return min + rand() % (max - min + 1);
+}
+
+// заполнение матрицы случайными значениями
+void fillMatrix(vector<vector<int>>& matrix) {
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                matrix[i][j] = 0;
+            }
+            else {
+                matrix[i][j] = randomInt(1, 100);
+            }
+        }
+    }
+}
