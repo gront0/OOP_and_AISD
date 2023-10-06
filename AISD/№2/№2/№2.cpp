640c5c106fd86b81c0ccddb188c7db1498ec3d58
gront0 <prizrak_fantoma@mail.ru>
2023-10-06 11:48:19 +0700

    Two

--- AISD/№2/№2/№2.cpp
+++ AISD/№2/№2/№2.cpp
@@ -23,6 +23,28 @@ vector<int> BoyerMooreSearch(const string& text, const string& pattern) {
 
     int s = 0;
 
+    while (s <= n - m) {
+        int j = m - 1;
+
+        while (j >= 0 && pattern[j] == text[s + j]) {
+            j--;
+        }
+
+        if (j < 0) {
+            positions.push_back(s);
+
+            if (s + m < n) {
+                s += m - badChar[text[s + m]];
+            }
+            else {
+                s += 1;
+            }
+        }
+        else {
+            s += max(1, j - badChar[text[s + j]]);
+        }
+    }
+
     return positions;
 }
 
@@ -33,6 +55,13 @@ int main() {
 
     int index = BoyerMooreSearch(text, pattern)[0];
     cout << "Индекс первого появления: " << index << endl;
+
+    vector<int> indexes = BoyerMooreSearch(text, pattern);
+    cout << "Индексы всех вхождений: ";
+
+    for (int i = 0; i < indexes.size(); i++) {
+        cout << indexes[i] << " ";
+    }
 
     cout << endl;
 
