93689ac5821dbc292bd8c9e97ae02e1022d533d4
gront0 <prizrak_fantoma@mail.ru>
2023-10-12 12:09:43 +0700

    Update №1_2.cpp

--- AISD/№1_2/№1_2/№1_2.cpp
+++ AISD/№1_2/№1_2/№1_2.cpp
@@ -10,7 +10,27 @@ int main() {
     cout << "Введите количество городов: ";  cin >> n;
     cout << endl;
 
+    vector<vector<int>> distance(n, vector<int>(n)); // матрица расстояний между городами
 
+    // Генерация случайной стоимости маршрута
+    srand(time(0));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i != j) {
+                distance[i][j] = rand() % 100 + 1; // случайное число от 1 до 100
+            }
+        }
+    }
+
+    // Вывод матрицы расстояний
+    cout << "Матрица расстояний:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << distance[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
