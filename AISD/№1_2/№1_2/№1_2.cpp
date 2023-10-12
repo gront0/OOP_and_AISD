3f9a791eccb32156b2c471d5ce4aff5de6aaa3ee
gront0 <prizrak_fantoma@mail.ru>
2023-10-12 12:08:43 +0700

    Create №1_2.cpp

---
+++ AISD/№1_2/№1_2/№1_2.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n; // количество городов
+    cout << "Введите количество городов: ";  cin >> n;
+    cout << endl;
+
+
+
+    return 0;
+}
