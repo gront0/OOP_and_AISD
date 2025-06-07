e70235a149f400918e8fe8b028524608f446154a
gront0 <prizrak_fantoma@mail.ru>
2025-06-07 16:35:13 +0700

    upload actual

---
+++ ООP/№3/№3/BoolVector.h
@@ -0,0 +1,12 @@
+#pragma once
+
+
+#include <iostream>
+#include <algorithm>
+
+
+class BoolVector {
+private:
+    bool* data;
+    size_t size;
+};
--- ООP/№3/№3/№3.cpp
+++ ООP/№3/№3/№3.cpp
@@ -1,1 +1,8 @@
-﻿
+﻿#include <iostream>
+#include "BoolVector.h"
+
+int main() {
+    std::cout << " > Конструктор по умолчанию:\n";
+    
+    return 0;
+}
