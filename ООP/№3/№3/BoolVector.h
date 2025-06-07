e8d858b07c422201228c37d389a51814edb2c48a
gront0 <prizrak_fantoma@mail.ru>
2025-06-07 16:51:26 +0700

    final update

--- ООP/№3/№3/BoolVector.h
+++ ООP/№3/№3/BoolVector.h
@@ -19,7 +19,6 @@ private:
         return len;
     }
 
-
     // добавление одного бита в конец (для ввода)
     void append(bool bit) {
         bool* new_data = new bool[size + 1];
@@ -60,4 +59,124 @@ public:
         delete[] data;
     }
 
+    // Размер
+    size_t getSize() const noexcept { return size; }
+
+    // Обмен
+    void swap(BoolVector& o) noexcept {
+        bool* tmp = data;
+        data = o.data;
+        o.data = tmp;
+
+
+        size_t s = size;
+        size = o.size;
+        o.size = s;
+    }
+
+
+    // Доступ по индексу (только чтение)
+    bool operator[](size_t i) const { return data[i]; }
+
+
+    // Ввод/вывод
+    friend std::istream& operator>>(std::istream& is, BoolVector& v) {
+        delete[] v.data;
+        v.data = nullptr;
+        v.size = 0;
+        char c;
+        while (is.get(c) && (c == '0' || c == '1')) {
+            v.append(c == '1');
+        }
+        if (is) is.unget();
+        return is;
+    }
+
+
+    friend std::ostream& operator<<(std::ostream& os, const BoolVector& v) {
+        for (size_t i = 0; i < v.size; ++i)
+            os << (v.data[i] ? '1' : '0');
+        return os;
+    }
+
+
+    // Инверсия всего вектора
+    void invert_all() noexcept {
+        for (size_t i = 0; i < size; ++i) data[i] = !data[i];
+    }
+
+    // Инверсия i-го бита
+    void invert(size_t i) { data[i] = !data[i]; }
+
+    // Установка i-го бита
+    void set(size_t i, bool value) { data[i] = value; }
+
+    // Вес (количество единиц)
+    size_t weight() const noexcept {
+        size_t cnt = 0;
+        for (size_t i = 0; i < size; ++i) if (data[i]) ++cnt;
+        return cnt;
+    }
+
+    // Побитовые операции
+    friend BoolVector operator&(const BoolVector& a, const BoolVector& b) {
+        size_t n = a.size < b.size ? a.size : b.size;
+        BoolVector r(n, false);
+        for (size_t i = 0; i < n; ++i) r.data[i] = a.data[i] & b.data[i];
+        return r;
+    }
+
+    friend BoolVector operator|(const BoolVector& a, const BoolVector& b) {
+        size_t n = a.size < b.size ? a.size : b.size;
+        BoolVector r(n, false);
+        for (size_t i = 0; i < n; ++i) r.data[i] = a.data[i] | b.data[i];
+        return r;
+    }
+
+    friend BoolVector operator^(const BoolVector& a, const BoolVector& b) {
+        size_t n = a.size < b.size ? a.size : b.size;
+        BoolVector r(n, false);
+        for (size_t i = 0; i < n; ++i) r.data[i] = a.data[i] ^ b.data[i];
+        return r;
+    }
+
+
+    friend BoolVector operator<<(const BoolVector& v, size_t s) {
+        BoolVector r(v.size, false);
+        for (size_t i = s; i < v.size; ++i) r.data[i] = v.data[i - s];
+        return r;
+    }
+
+
+    friend BoolVector operator>>(const BoolVector& v, size_t s) {
+        BoolVector r(v.size, false);
+        for (size_t i = 0; i + s < v.size; ++i) r.data[i] = v.data[i + s];
+        return r;
+    }
+
+    friend BoolVector operator~(const BoolVector& v) {
+        BoolVector r(v);
+        r.invert_all();
+        return r;
+    }
+
+    // Присваивание
+    BoolVector& operator=(const BoolVector& o) {
+        if (this != &o) {
+            BoolVector tmp(o);
+            swap(tmp);
+        }
+        return *this;
+    }
+
+    BoolVector& operator=(BoolVector&& o) noexcept {
+        if (this != &o) {
+            delete[] data;
+            data = o.data;
+            size = o.size;
+            o.data = nullptr;
+            o.size = 0;
+        }
+        return *this;
+    }
 };
--- ООP/№3/№3/№3.cpp
+++ ООP/№3/№3/№3.cpp
@@ -1,8 +1,71 @@
 ﻿#include <iostream>
 #include "BoolVector.h"
+#include <locale.h>
 
 int main() {
-    std::cout << " > Конструктор по умолчанию:\n";
-    
+    setlocale(LC_ALL, "Russian");
+    std::cout << "1) Конструктор по умолчанию:\n";
+    BoolVector v1;
+    std::cout << "v1 (пустой): '" << v1 << "' размер=" << v1.getSize() << "\n\n";
+
+    std::cout << "2) Конструктор (размер, значение):\n";
+    BoolVector v2(5, true);
+    std::cout << "v2 (5 единиц): '" << v2 << "' размер=" << v2.getSize() << "\n\n";
+
+    std::cout << "3) Конструктор из C-строки:\n";
+    BoolVector v3("10110");
+    std::cout << "v3 ('10110'): '" << v3 << "' размер=" << v3.getSize() << "\n\n";
+
+    std::cout << "4) Конструктор копирования и оператор присваивания:\n";
+    BoolVector v4(v3);
+    BoolVector v5;
+    v5 = v2;
+    std::cout << "v4 (копия v3): '" << v4 << "'\n";
+    std::cout << "v5 (присвоено от v2): '" << v5 << "'\n\n";
+
+    std::cout << "5) Доступ по индексу (operator[]):\n";
+    std::cout << "v3[2] = " << v3[2] << "\n\n";
+
+    std::cout << "6) Инверсия всех битов (invert_all()):\n";
+    v3.invert_all();
+    std::cout << "v3 инвертирован: '" << v3 << "'\n\n";
+
+    std::cout << "7) Инверсия одного бита (invert(i)) и установка значения (set(i)):\n";
+    v3.invert(1);
+    std::cout << "v3 после invert(1): '" << v3 << "'\n";
+    v3.set(2, true);
+    std::cout << "v3 после set(2, true): '" << v3 << "'\n\n";
+
+    std::cout << "8) Вес (количество единиц):\n";
+    std::cout << "v3 вес = " << v3.weight() << "\n\n";
+
+    std::cout << "9) Обмен содержимым (swap()):\n";
+    std::cout << "До swap v2='" << v2 << "', v3='" << v3 << "'\n";
+    v2.swap(v3);
+    std::cout << "После swap v2='" << v2 << "', v3='" << v3 << "'\n\n";
+
+    std::cout << "10) Побитовые операции & | ^ :\n";
+    BoolVector va("1100");
+    BoolVector vb("1010");
+    std::cout << "va = '" << va << "', vb='" << vb << "'\n";
+    std::cout << "va & vb = '" << (va & vb) << "'\n";
+    std::cout << "va | vb = '" << (va | vb) << "'\n";
+    std::cout << "va ^ vb = '" << (va ^ vb) << "'\n\n";
+
+    std::cout << "11) Побитовые сдвиги << >> :\n";
+    BoolVector vs("1001");
+    std::cout << "vs='" << vs << "'\n";
+    std::cout << "vs << 2 = '" << (vs << 2) << "'\n";
+    std::cout << "vs >> 1 = '" << (vs >> 1) << "'\n\n";
+
+    std::cout << "12) Побитовая инверсия (~):\n";
+    std::cout << "~vs = '" << (~vs) << "'\n\n";
+
+    std::cout << "13) Ввод с консоли (введите биты):\n";
+    BoolVector vin;
+    std::cout << "Введите последовательность битов (например, 010101), затем пробел или Enter: ";
+    std::cin >> vin;
+    std::cout << "Вы ввели: '" << vin << "' размер=" << vin.getSize() << "\n";
+
     return 0;
 }
