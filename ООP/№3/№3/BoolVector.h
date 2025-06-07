66693428b4afd33b42850a081703924ec0e28787
gront0 <prizrak_fantoma@mail.ru>
2025-06-07 16:41:20 +0700

    constructors upload

--- ООP/№3/№3/BoolVector.h
+++ ООP/№3/№3/BoolVector.h
@@ -12,6 +12,14 @@ private:
 
 
 private:
+    // вычисление длины C-строки
+    static size_t cstr_len(const char* s) {
+        size_t len = 0;
+        while (s[len] != '\0') ++len;
+        return len;
+    }
+
+
     // добавление одного бита в конец (дл€ ввода)
     void append(bool bit) {
         bool* new_data = new bool[size + 1];
@@ -21,4 +29,35 @@ private:
         data = new_data;
         ++size;
     }
+
+public:
+    //  онструкторы
+    BoolVector() : data(nullptr), size(0) {}
+
+    BoolVector(size_t n, bool value) : data(new bool[n]), size(n) {
+        for (size_t i = 0; i < n; ++i) data[i] = value;
+    }
+
+    BoolVector(const char* cstr) : data(nullptr), size(0) {
+        size_t len = cstr_len(cstr);
+        data = new bool[len];
+        size = len;
+        for (size_t i = 0; i < size; ++i) {
+            data[i] = (cstr[i] == '1');
+        }
+    }
+
+    BoolVector(const BoolVector& other) : data(new bool[other.size]), size(other.size) {
+        for (size_t i = 0; i < size; ++i) data[i] = other.data[i];
+    }
+
+    BoolVector(BoolVector&& other) noexcept : data(other.data), size(other.size) {
+        other.data = nullptr;
+        other.size = 0;
+    }
+
+    ~BoolVector() {
+        delete[] data;
+    }
+
 };
