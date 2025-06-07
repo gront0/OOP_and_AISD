5dca497c8989fe2ecbb05ddaf05cf6eb027c71be
gront0 <prizrak_fantoma@mail.ru>
2025-06-07 16:37:59 +0700

    write

--- ООP/№3/№3/BoolVector.h
+++ ООP/№3/№3/BoolVector.h
@@ -9,4 +9,16 @@ class BoolVector {
 private:
     bool* data;
     size_t size;
+
+
+private:
+    // добавление одного бита в конец (для ввода)
+    void append(bool bit) {
+        bool* new_data = new bool[size + 1];
+        for (size_t i = 0; i < size; ++i) new_data[i] = data[i];
+        new_data[size] = bit;
+        delete[] data;
+        data = new_data;
+        ++size;
+    }
 };
